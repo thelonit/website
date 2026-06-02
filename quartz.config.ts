0623683dd7b3442a1000e26cc8a6548ae1d6e56b
Lonit <67799863+thelonit@users.noreply.github.com>
2026-06-02 10:13:50 +0330

    Quartz sync: Jun 2, 2026, 10:13 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -471,6 +471,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
+      //Plugin.HardLineBreaks(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
